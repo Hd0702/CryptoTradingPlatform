ccf7efcd913d813e16dc71fe770b5809cded05a7
Hd0702 <haydendonofrio@gmail.com>
2023-09-30 17:48:02 -0500

    Remove cmake files from repo

--- tst/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/haydendonofrio/workplace/CryptoTradingPlatform/tst
-# Build directory: /Users/haydendonofrio/workplace/CryptoTradingPlatform/tst
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[CryptoTradingProject_tst]=] "/Users/haydendonofrio/workplace/CryptoTradingPlatform/tst/CryptoTradingProject_tst")
-set_tests_properties([=[CryptoTradingProject_tst]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/haydendonofrio/workplace/CryptoTradingPlatform/tst/CMakeLists.txt;18;add_test;/Users/haydendonofrio/workplace/CryptoTradingPlatform/tst/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
